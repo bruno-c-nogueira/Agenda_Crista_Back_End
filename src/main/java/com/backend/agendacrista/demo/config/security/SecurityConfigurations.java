e5e3731b8de5cbe07e107d699f46971e3d3aac0a
Laender Oliveira <laendermo@gmail.com>
2020-05-16 20:40:46 -0300

    Update SecurityConfigurations.java

--- src/main/java/com/backend/agendacrista/demo/config/security/SecurityConfigurations.java
+++ src/main/java/com/backend/agendacrista/demo/config/security/SecurityConfigurations.java
@@ -47,7 +47,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers( HttpMethod.POST,"/auth").permitAll()
-                .antMatchers(HttpMethod.POST,"/register/").permitAll()
+                .antMatchers(HttpMethod.POST,"/register").permitAll()
                 .antMatchers(HttpMethod.GET,"/register/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
